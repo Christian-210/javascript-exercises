feba160af9fc1460b88b4531b27c899196954a76
Christian-210 <workwithcaw@gmail.com>
2022-05-06 16:35:37 -0500

    Solve the problems

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,32 @@
-const ftoc = function() {
+const ftoc = function (temp) {
+  let tempC = (temp - 32) * (5 / 9);
 
+  if (!Number.isInteger(tempC)) {
+    return Number(tempC.toFixed(1));
+  }
+
+  return tempC;
 };
 
-const ctof = function() {
+const ctof = function (temp) {
+  let tempF = temp * (9 / 5) + 32;
 
+  if (!Number.isInteger(tempF)) {
+    return Number(tempF.toFixed(1));
+  }
+
+  return tempF;
 };
+
+// console.log(ftoc(32));
+// console.log(ftoc(100));
+// console.log(ftoc(-100));
+// console.log(ctof(0));
+// console.log(ctof(73.2));
+// console.log(ctof(-10));
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
