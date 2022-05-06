bb39ffd429c42da07db4591f52a551c0487fb49d
Christian-210 <workwithcaw@gmail.com>
2022-05-06 16:45:05 -0500

    Solved the problems

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  let newArr = [];
 
+  arr.forEach((item) => {
+    if (!args.includes(item)) {
+      newArr.push(item);
+    }
+  });
+
+  return newArr;
 };
+
+//console.log(removeFromArray([1, 2, 3, 4], 3));
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
