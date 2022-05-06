f35c917075b4025b9793e7b98ac58137fe462c7e
Christian-210 <workwithcaw@gmail.com>
2022-05-06 16:39:34 -0500

    Solved the problems

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+// Test #1
 
+const repeatString = function (str, num) {
+  let repeatedStr = "";
+
+  if (num < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < num; i++) {
+    repeatedStr += str;
+  }
+
+  return repeatedStr;
 };
 
 // Do not edit below this line
