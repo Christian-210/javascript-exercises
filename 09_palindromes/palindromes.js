3efc749c06a120e192a398cabce5aa0c164c4097
Christian-210 <workwithcaw@gmail.com>
2022-05-06 19:28:43 -0500

    Solve all problems

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+  const spaces = /\s+/g;
+  const newStr = str.replace(regex, "").replace(spaces, "").toLowerCase();
+  const pal = [...str]
+    .reverse()
+    .join("")
+    .replace(regex, "")
+    .replace(spaces, "")
+    .toLowerCase();
+  return newStr === pal;
+  //   console.log(newStr);
+  //   console.log(pal);
 };
+
+// console.log(palindromes("racecar"));
+// console.log(palindromes("racecar!"));
+// console.log(palindromes("Racecar!"));
+// console.log(palindromes("A car, a man, a maraca."));
+// console.log(palindromes("Animal loots foliated detail of stool lamina."));
+// console.log(palindromes("ZZZZ car, a man, a maracaz."));
 
 // Do not edit below this line
 module.exports = palindromes;
