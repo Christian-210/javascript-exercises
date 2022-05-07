334517872a0600d302c22ed9b978344582083e72
Christian-210 <workwithcaw@gmail.com>
2022-05-06 22:14:37 -0500

    Solved the problems

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,30 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
+// const getTheTitles = books.map((book) => book.title);
+
+// const getTheTitles = function (arr) {
+//   const bookTitles = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     bookTitles.push(arr[i].title);
+//   }
+
+//   return bookTitles;
+// };
+
+const getTheTitles = function (arr) {
+  return arr.map((book) => book.title);
 };
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
